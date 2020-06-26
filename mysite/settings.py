e8eb495e02495b8b076bf51f7c11b04bea216c91
Igor Belitskiy <i.belitskiy00@mail.ru>
2020-06-26 17:36:05 +0600

    REST API_2

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,12 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 # Application definition
