5b3a32bb6357c741faa81962d9543e397d1a0814
Igor Belitskiy <i.belitskiy00@mail.ru>
2020-07-09 13:56:24 +0600

    Regis d.2

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,13 +48,8 @@ INSTALLED_APPS = [
     'regis.apps.RegisConfig',
 
 ]
+#AUTH_USER_MODEL = 'core.User'
 
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- regis/views.py
+++ regis/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
+from django.core import serializers
 
 
 def index(request):
     if request.method == "POST":
         user = User(request.POST)
-        if user.is_valid():
-            name = user.cleaned_data["username"]#Находится в классе django AbstractUser username
 
-            return HttpResponse("<h2>Hello, {0}</h2>".format(name))
+
+        if user.is_valid():
+            name = user.cleaned_data["username"]
+
+            user = serializers.serialize('xml', user.objects.all())
+
+            user.set_password(user.password)
+            user.save()
+
+            return HttpResponse("<h2>Данные правильные, {0}</h2>".format(name))
         else:
-            return HttpResponse("Invalid data")
+            return HttpResponse("Ошибка ввода")
     else:
         user = User()
         return render(request, "index.html", {"form": user})
