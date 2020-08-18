02fde6ebd2055ca39e30b94ecb70a12573f48a56
igor <goga.belitskiy.00@mail.ru>
2020-08-18 10:51:38 +0600

    practice

--- blog/views.py
+++ blog/views.py
@@ -23,6 +23,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+
 def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
